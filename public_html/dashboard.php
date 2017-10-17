9bc9da97f6719fbb9213e37b00b12ba58682f66a
MartinR01 <martin.ryba01@gmail.com>
2017-10-17 23:19:54 +0200

    added login page to index, dashboard now in dashboard.php

--- public_html/dashboard.php
+++ public_html/dashboard.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
-<!-- DASHBOARD
-		display journeys,...
+<!-- LOGIN SCREEN
+		if user already logged in, redirect to student/teacher dashboard
 -->
 <html>
 <head>
-    <meta charset="utf-8">
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Quest up</title>
-
-    <!-- CSS Our-->
-    <link rel="stylesheet" href="css/design.css">
     <!-- CSS Bootstrap -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap.css">
+      <link rel="stylesheet" href="css/bootstrap.css">
+      <!-- CSS Our-->
+      <link rel="stylesheet" href="css/design.css">
     <!-- jQuery and theamJQuery comented-->
     <!--link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css" type="text/css" /-->
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
@@ -19,40 +20,53 @@
 </head>
 
 <body>
-	<div class="container">
-		<!-- SIDEBAR -->
-		<div class="col-sm-3" style="background-color: green">
-			<!-- UPPER PART - progress -->
-			<div class="row">
-				<!-- XP -->
-				<div class="row">
-				
-				</div>
-				<!-- JOURNEY PROGRESS -->
-				<div class="row">
-				
-				</div>
-			</div>
-			<!-- NAVIGATION -->
-			<div class="row">
-				<div class="navbar navbar-default" style="background-color: pink">
-					
-					<ul class="nav nav-pills nav-stacked">
-						<li class="active">flsfjslfja</li>
-						<li class="">flsfjslfja</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-		<!-- MAIN SECTION -->
-		<div class="col-sm-9" style="background-color: blue">
-			Main section
-		</div>
-	</div>
-	
-	
+	 <div class="row affix-row">
+     <!--/col - left -->
+	    <div class="col-sm-3 col-md-3 affix-sidebar">
+        <!--/sidebar-nav -->
+        <div class="sidebar-nav  ">
+          <!--/.nav-normal -->
+          <div class=" navbar navbar-default" role="navigation">
+            <div class=" color navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <span class="visible-xs color navbar-brand">Home Menu</span>
+            </div>
+            <!--/.nav-collapse -->
+            <div class="navbar-collapse collapse sidebar-navbar-collapse">
+              <ul class="nav navbar-nav" id="sidenav01">
+
+                <li class="timecolor"><a href="#">
+                  <img class="imgprofile" src="img/profileTest.jpg" width="150px" height="150px" alt="">
+                  <h4 class= "proname">
+                    Name Lastname<br><small>Student</small>
+                  </h4>
+
+
+              </a></li>
+                <li><a href="index.php"> Dashboard </a></li>
+                <li><a href="#">Journey </span></a></li>
+                <li><a href="#">Quests</a></li>
+                <li><a href="#"> Notification <span class="badge pull-right">42</span></a></li>
+
+              </ul>
+            </div><!--/.nav-collapse -->
+          </div><!--/.nav-normal -->
+        </div><!--/sidebar-nav -->
+	    </div><!--/col - left -->
+
+      <div class="col-sm-9 col-md-9 affix-content">
+        <h1> Heyy </h1>
+
+      </div>
+	 </div>
     <!-- JS for Bootstrap -->
-  <script src="js/bootstrap.min.js"></script>
+      <script src="js/bootstrap.js"></script>
   <script src="js/bootstrap.js"></script>
 </body>
 </html>
+
--- public_html/index.php
+++ public_html/index.php
@@ -4,15 +4,14 @@
 -->
 <html>
 <head>
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
     <title>Quest up</title>
+
+    <!-- CSS Our-->
+    <link rel="stylesheet" href="css/design.css">
     <!-- CSS Bootstrap -->
-      <link rel="stylesheet" href="css/bootstrap.css">
-      <!-- CSS Our-->
-      <link rel="stylesheet" href="css/design.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
     <!-- jQuery and theamJQuery comented-->
     <!--link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css" type="text/css" /-->
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
@@ -20,53 +19,19 @@
 </head>
 
 <body>
-	 <div class="row affix-row">
-     <!--/col - left -->
-	    <div class="col-sm-3 col-md-3 affix-sidebar">
-        <!--/sidebar-nav -->
-        <div class="sidebar-nav  ">
-          <!--/.nav-normal -->
-          <div class=" navbar navbar-default" role="navigation">
-            <div class=" color navbar-header">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <span class="visible-xs color navbar-brand">Home Menu</span>
-            </div>
-            <!--/.nav-collapse -->
-            <div class="navbar-collapse collapse sidebar-navbar-collapse">
-              <ul class="nav navbar-nav" id="sidenav01">
-
-                <li class="timecolor"><a href="#">
-                  <img class="imgprofile" src="img/profileTest.jpg" width="150px" height="150px" alt="">
-                  <h4 class= "proname">
-                    Name Lastname<br><small>Student</small>
-                  </h4>
-
-
-              </a></li>
-                <li><a href="index.php"> Dashboard </a></li>
-                <li><a href="#">Journey </span></a></li>
-                <li><a href="#">Quests</a></li>
-                <li><a href="#"> Notification <span class="badge pull-right">42</span></a></li>
-
-              </ul>
-            </div><!--/.nav-collapse -->
-          </div><!--/.nav-normal -->
-        </div><!--/sidebar-nav -->
-	    </div><!--/col - left -->
-
-      <div class="col-sm-9 col-md-9 affix-content">
-        <h1> Heyy </h1>
-
-      </div>
-	 </div>
+	<div class="container">
+		<div class="row centered">
+			<div class="col-sm-4 col-sm-offset-4 text-center" id="login" style="background-color: green">
+				<form action="dashboard.php">
+					<input name="email" type="text"/>
+					<input name="password" type="password"/>
+					<input name="login" type="submit"/>
+				</form>
+			</div>
+		</div>
+	</div>
     <!-- JS for Bootstrap -->
-      <script src="js/bootstrap.js"></script>
+  <script src="js/bootstrap.min.js"></script>
   <script src="js/bootstrap.js"></script>
 </body>
 </html>
-
