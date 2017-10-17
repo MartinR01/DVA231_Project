f462e6e169ed3488947df4884c135ac866a771f1
MartinR01 <martin.ryba01@gmail.com>
2017-10-17 17:52:30 +0200

    basic blocks for sidebar

---
+++ public_html/dashboard.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!-- DASHBOARD
+		display journeys,...
+-->
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Quest up</title>
+
+    <!-- CSS Our-->
+    <link rel="stylesheet" href="css/design.css">
+    <!-- CSS Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- jQuery and theamJQuery comented-->
+    <!--link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css" type="text/css" /-->
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+
+</head>
+
+<body>
+	<div class="container">
+		<div class="col-sm-3" style="background-color: green">
+			<div class="row">
+			</div>
+			<div class="row">
+				<div class="navbar navbar-default" style="background-color: pink">
+					
+					<ul class="nav nav-pills nav-stacked">
+						<li class="active">flsfjslfja</li>
+						<li class="">flsfjslfja</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-9" style="background-color: blue">
+			Main section
+		</div>
+	</div>
+	
+	
+    <!-- JS for Bootstrap -->
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/bootstrap.js"></script>
+</body>
+</html>
