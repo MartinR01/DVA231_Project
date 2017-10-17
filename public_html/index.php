87db3c42b9e078e47decf8a2c1236dd9a699c539
Djordje Kalezic <djordjekalezic@108.90.243.130-v831.wireless.mdh.se>
2017-10-17 12:17:06 +0200

    lol

--- public_html/index.php
+++ public_html/index.php
@@ -4,23 +4,11 @@
     <meta charset="utf-8">
     <title>Quest up</title>
 
-    <!-- CSS Our-->
-    <link rel="stylesheet" href="/css/design.css">
-    <!-- CSS Bootstrap -->
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/bootstrap.css">
-    <!-- jQuery and theamJQuery comented-->
-    <!--link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css" type="text/css" /-->
-    <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-
+    <
 </head>
 
 <body>
-    <h1>Lets Start Today </h1>
-
-
-    <!-- JS for Bootstrap -->
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/bootstrap.js"></script>
+    <h1>Hello RPG</h1>
+    <p>This file is ftp testing</p>
 </body>
 </html>
