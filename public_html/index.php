ea2770f0cffc6ab5eac3592b54bab959f1025da0
Anemone Kampkötter <anemone@kampkoetter.de>
2017-10-17 23:10:18 +0200

    update version

--- public_html/index.php
+++ public_html/index.php
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<!-- LOGIN SCREEN
-		if user already logged in, redirect to student/teacher dashboard
--->
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Quest up</title>
-
-    <!-- CSS Our-->
-    <link rel="stylesheet" href="css/design.css">
-    <!-- CSS Bootstrap -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <!-- jQuery and theamJQuery comented-->
-    <!--link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css" type="text/css" /-->
-    <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-
-</head>
-
-<body>
-	<div class="container">
-		<div class="row centered">
-			<a href="dashboard.php">
-				<div class="col-sm-4 col-sm-offset-4 text-center" id="login" style="background-color: green">
-					<h1 class = "align-middle">LOGIN</h1>
-				</div>
-			</a>
-		</div>
-	</div>
-    <!-- JS for Bootstrap -->
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/bootstrap.js"></script>
-</body>
-</html>
-=======
 <!DOCTYPE html>
 <!-- LOGIN SCREEN
 		if user already logged in, redirect to student/teacher dashboard
@@ -106,4 +69,4 @@
   <script src="js/bootstrap.js"></script>
 </body>
 </html>
->>>>>>> 39370e63148617d6c3d3aedeef929d9a6fbc948a
+
