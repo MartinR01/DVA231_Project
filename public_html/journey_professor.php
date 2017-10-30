e3ca3ae2924f736adcdf00ddb7189fdf09941a3f
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2017-10-30 18:33:37 +0100

    Change button color

--- public_html/journey_professor.php
+++ public_html/journey_professor.php
@@ -154,7 +154,8 @@ session_start();
 
       <div class="container" align="center">
       <div class="row">
-        <a href="edit_journey.php"><button type="button" class="btn-add-journey shadow"><i class="material-icons">add</i></button></a>
+        <a href="edit_journey.php"><button type="button" class="btn-add-journey shadow" id="green-button"><i class="material-icons">add</i></button></a>
+
       </div>
     </div>
     </div>
