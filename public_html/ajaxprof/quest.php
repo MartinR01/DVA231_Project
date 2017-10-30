1b4fb34f5a761d744418902b4109161cef50fa76
MartinR01 <martin.ryba01@gmail.com>
2017-10-30 19:05:32 +0100

    sdfdfhs4

--- public_html/ajaxprof/quest.php
+++ public_html/ajaxprof/quest.php
@@ -1,5 +1,7 @@
 <?php
   $journey = $_COOKIE['journey'];
+  $QUESTS_PER_ROW = 4;
+  $QUEST_COLS = 12 / $QUESTS_PER_ROW;
  ?>
         <div class="row">
           <div class="dropdown">
@@ -15,29 +17,47 @@
         <hr/>
         <?php
 
-         $sql = "select idquest,title,description,questm from quest where idjourn = $journey";
+         $sql_main = "select idquest,title,description from quest where idjourn = $journey AND questm = 'main' ORDER BY sortnum ASC";
+		 $sql_side = "select idquest,title,description from quest where idjourn = $journey AND questm = '' ORDER BY sortnum ASC";
 
          ?>
       <div class="container">
+		<h1>Main Quests</h1>
         <div class="row"><!--quest row -->
           <?php
           require_once('../protected/config.php');
-          $result = mysqli_query($connection,$sql);
-		  $quest_type;
-          if (mysqli_num_rows($result) > 0) {
-            while($row = mysqli_fetch_assoc($result)) {
-				$quest_type = ($row['questm'] == "") ? "sidequest":"quest";
+          $result_main = mysqli_query($connection,$sql_main);
+		  $result_side = mysqli_query($connection,$sql_side);
+          if (mysqli_num_rows($result_main) > 0) {
+            while($mains = mysqli_fetch_assoc($result_main)) {
            ?>
-          <div class="col-sm-4 <?php echo $quest_type; ?>-container" align="center"><!--Quest 1 -->
-            <div class="<?php echo $quest_type; ?>-availability"></div>
-            <div class="<?php echo $quest_type; ?> shadow" data-toggle="tooltip" title="<?php echo $row['description']; ?>" data-placement="right">
-				<h4><?php echo $row['title'];?></h4>
+          <div class="col-sm-<?php echo $QUEST_COLS ?> quest-container" align="center"><!--Quest 1 -->
+            <div class="quest-availability"></div>
+            <div class="quest shadow" data-toggle="tooltip" title="<?php echo $mains['description']; ?>" data-placement="right">
+				<h4><?php echo $mains['title'];?></h4>
 				
             </div>
           </div><!--Quest 1 end-->
           <?php
-        }
-      }else {
+			}?>
+			</div>
+			<hr/>
+			<h1>Side Quests</h1>
+			<div class="row">
+			<?php
+			while($side = mysqli_fetch_assoc($result_side)){?>
+			<div class="col-sm-<?php echo $QUEST_COLS ?> sidequest-container" align="center"><!--Quest 1 -->
+            <div class="sidequest-availability"></div>
+            <div class="sidequest shadow" data-toggle="tooltip" title="<?php echo $side['description']; ?>" data-placement="right">
+				<h4><?php echo $side['title'];?></h4>
+				
+            </div>
+          </div><!--Quest 1 end-->
+		<?php		
+		}
+			
+      }
+		else {
         ?>
         <h1> ADD quest !</h1>
         <?php
