88d0d20bf3c81692e1bb9a93db2b2a82bad981c1
Djordje Kalezic <djordjekalezic@79.90.243.130-v831.wireless.mdh.se>
2017-11-05 15:38:07 +0100

    To internet

--- public_html/protected/config.php
+++ public_html/protected/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DBHOST', 'localhost');
 define('DBNAME', 'id3050670_gameplatform');
-define('DBUSER', 'root');
-define('DBPASS', '');
+define('DBUSER', 'id3050670_fancygroup');
+define('DBPASS', 'fancy123');
 
 $connection =mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
 ?>
